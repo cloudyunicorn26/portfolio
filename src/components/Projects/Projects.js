fb919095d25cbb6c6e4cbc8e4afd648c393c7408
Rajat Singh <s.rajat55@gmail.com>
2023-02-13 17:54:58 +0530

    added projects
    Signed-off-by: Rajat Singh <s.rajat55@gmail.com>

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import leonardo247 from "../../Assets/Projects/leonardo247.png";
+// import emotion from "../../Assets/Projects/emotion.png";
+import ziplingo from "../../Assets/Projects/ziplingo.png";
+import cryptotrak from "../../Assets/Projects/CryptoTrak.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+import dashboard from "../../Assets/Projects/dashboard.png";
 
 function Projects() {
   return (
@@ -23,49 +23,47 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={cryptotrak}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="CrptoTrak"
+              description="A cryptocurrency tracking website to stay informed with accurate and up-to-date information about performance of varous cryptocurrencies. Made with React JS, Material UI and chart.js"
+              ghLink="https://github.com/cloudyunicorn/CryptoTrak"
+              demoLink="https://crypto-trak.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={dashboard}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Dashboard for Krenai"
+              description="A complete dashboard design using Angular, Angular Material and Angular flex for Krenai solutions pvt. ltd. showing all the neccessary statistics like sales, inventory, customers, performance etc."
+              ghLink="https://github.com/cloudyunicorn/krenai-dashboard"
+              demoLink="https://krenai-dashboard.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={ziplingo}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="UI for Ziplingo"
+              description="Ziplingo is a messaging platform solution for businesses to engage with distributors and customers accross the world. Designed main UI pages for the company including Dashboard, Contacts, Campaigns etc."
+              demoLink="https://ziplingo.com"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={leonardo247}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Leonardo247"
+              description="A property operations and maintenance platform to streamline and simplify tasks for property owners and helpers. Worked on react forms, UI changes, page layouts using React,js and third party react libraries like react-select, react-draggable."
+              demoLink="https://leonardo247.com/"
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
@@ -86,7 +84,7 @@ function Projects() {
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </Container>
